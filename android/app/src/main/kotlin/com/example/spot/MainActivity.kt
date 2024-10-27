47874773a49a5dc5def4c89feacea28cf8a0fcee
Matheus Andretti <andrettiguermo@gmail.com>
2024-10-27 17:31:49 -0300

    2710

--- android/app/src/main/kotlin/com/example/spot/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/spot/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.example.spot
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity() {
+    // Nenhuma implementação adicional é necessária aqui.
+}
